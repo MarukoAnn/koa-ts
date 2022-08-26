682140cdff0c1262c14048f5653ba2af6c3a34c0
张贵安 <zhangguian@chinasofti.com>
2022-08-26 10:05:15 +0800

    :sparkles: feat: 新增代码效验提交规范

--- src/controller/homeController.ts
+++ src/controller/homeController.ts
@@ -1,23 +1,21 @@
-
 import Auth from '../middlewares/Auth'
 import HomeService from '../service/HomeService'
-import { ResponseMsg, parseTokenInfo } from '../utils/utils';
-const auth = new Auth();
+import { ResponseMsg, parseTokenInfo } from '../utils/utils'
+const auth = new Auth()
 export default class HelloController {
-    private service: HomeService = new HomeService();
-    public hello = async (ctx: any) => {
-        console.log('进来了？')
-        console.log('cookies', ctx.cookies.get('token'))
-        console.log(ctx.request.header['authorization'], 'authorization')
-        let info = auth.verify(parseTokenInfo(ctx.request.header['authorization']));
-        // if(info['username'] == 'admin') {
-            console.log('123')
-        // }
-        let data = {
-            code: 200,
-            msg: '请求成功',
-            data: info,
-        }
-        ctx.body = data;
+  private service: HomeService = new HomeService()
+  public hello = async (ctx: any) => {
+    console.log('cookies', ctx.cookies.get('token'))
+    console.log(ctx.request.header['authorization'], 'authorization')
+    let info = auth.verify(parseTokenInfo(ctx.request.header['authorization']))
+    // if(info['username'] == 'admin') {
+    console.log('123')
+    // }
+    let data = {
+      code: 200,
+      msg: '请求成功',
+      data: info,
     }
+    ctx.body = data
+  }
 }
